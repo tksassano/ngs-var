19afff750655355b9c626fee6bde518dbf156463
tksassano <113140378+tksassano@users.noreply.github.com>
2023-07-07 22:39:27 -0400

    Update arg_test.py

--- arg_test.py
+++ arg_test.py
@@ -12,72 +12,76 @@ import ngs_classes
 
 def check_inputs(inputs):
 	
-	if inputs.verbose: print "[INFO] Checking provided arguments"
-	
-	if inputs.trimmer == 'CUTADAPT':
+	if inputs.verbose: print("[INFO] Checking provided arguments")
+	if inputs.trimmer == 'FASTP':
 		if(os.path.exists(inputs.cutadapt)):
-			if inputs.verbose: print "[INFO:CUTADAPT] Available"
+			if inputs.verbose: print("[INFO:FASTP] Available")
 		else:
-			if inputs.verbose: print "[ERROR:CUTADAPT] Not Available"
+			if inputs.verbose: print("[ERROR:FASTP] Not Available")
+			return(None)
+	elif inputs.trimmer == 'CUTADAPT':
+		if(os.path.exists(inputs.cutadapt)):
+			if inputs.verbose: print("[INFO:CUTADAPT] Available")
+		else:
+			if inputs.verbose: print("[ERROR:CUTADAPT] Not Available")
 			return(None)
 	elif inputs.trimmer == 'AFTERQC':
 		if(os.path.exists(inputs.afterqc)):
-			if inputs.verbose: print "[INFO:AFTERQC] Available"
+			if inputs.verbose: print("[INFO:AFTERQC] Available")
 		else:
-			if inputs.verbose: print "[ERROR:AFTERQC] Not Available"
+			if inputs.verbose: print("[ERROR:AFTERQC] Not Available")
 			return(None)
 	elif inputs.trimmer == 'SKEWER':
 		if(os.path.exists(inputs.skewer)):
-			if inputs.verbose: print "[INFO:SKEWER] Available"
+			if inputs.verbose: print("[INFO:SKEWER] Available")
 		else:
-			if inputs.verbose: print "[ERROR:SKEWER] Not Available"
+			if inputs.verbose: print("[ERROR:SKEWER] Not Available")
 			return(None)
 	
 	if not (inputs.aligner == 'BWA' or inputs.aligner == 'BOWTIE2'):
-		if inputs.verbose: print "[ERROR] Name of the aligner does not match available tools"
+		if inputs.verbose: print("[ERROR] Name of the aligner does not match available tools")
 		return(None)
 	else:
 		if inputs.aligner == 'BWA':
 			if(os.path.exists(inputs.aligner)):
-				if inputs.verbose: print "[INFO:BWA] Available"
+				if inputs.verbose: print("[INFO:BWA] Available")
 			else:
-				if inputs.verbose: print "[ERROR:BWA] Not Available"
+				if inputs.verbose: print("[ERROR:BWA] Not Available")
 				return(None)
 		elif inputs.aligner == 'BOWTIE2':
 			if(os.path.exists(inputs.bowtie2)):
-				if inputs.verbose: print "[INFO:BOWTIE2] Available"
+				if inputs.verbose: print("[INFO:BOWTIE2] Available")
 			else:
-				if inputs.verbose: print "[ERROR:BOWTIE2] Not Available"
+				if inputs.verbose: print("[ERROR:BOWTIE2] Not Available")
 				return(None)
 
 	if inputs.workflow == 'GERMLINESNVINDEL':
 		if not (inputs.tool == 'HAPLOTYPECALLER' or inputs.tool == 'STRELKA2'):
-			if inputs.verbose: print "[ERROR] Name of the caller does not match available tools"
+			if inputs.verbose: print("[ERROR] Name of the caller does not match available tools")
 			return(None)
 	elif inputs.workflow == 'SOMATICSNVINDEL':
 		if not (inputs.tool == 'STRELKA2'):
-			if inputs.verbose: print "[ERROR] Name of the caller does not match available tools"
+			if inputs.verbose: print("[ERROR] Name of the caller does not match available tools")
 			return(None)
 	else:
-		if inputs.verbose: print "[ERROR] Name of the workflow does not match available workflows"
+		if inputs.verbose: print("[ERROR] Name of the workflow does not match available workflows")
 	
 	for tool in [inputs.gatk,
-				 inputs.picard,
 				 inputs.sambamba]:
 		if(os.path.exists(os.path.abspath(tool)) and os.access(tool, os.R_OK)):
-			if inputs.verbose: print "[INFO] Available: %s" % (tool)
+			if inputs.verbose: print("[INFO] Available: %s" % (tool))
 		else:
-			if inputs.verbose: print "[ERROR] Not Available: %s, exiting" % (tool)
+			if inputs.verbose: print("[ERROR] Not Available: %s, exiting" % (tool))
 			return(None)
 	
 	for knownSite in inputs.knownsites:
 		if(os.path.exists(os.path.abspath(knownSite)) and os.access(knownSite, os.R_OK)):
-			if inputs.verbose: print "[INFO] Available %s" % (knownSite)
+			if inputs.verbose: print("[INFO] Available %s" % (knownSite))
 		else:
-			if inputs.verbose: print "[ERROR] Not Available: %s, exiting" % (knownSite)
+			if inputs.verbose: print("[ERROR] Not Available: %s, exiting" % (knownSite))
 			return(None)
 		
-	if inputs.verbose: print "[INFO] Done."
+	if inputs.verbose: print("[INFO] Done.")
 		
 	return(True)
 
@@ -85,7 +89,7 @@ def parse_config(config_file, inputs):
 	
 	config_count = 0
 	if not(os.path.exists(os.path.abspath(config_file))):
-		print "\n!Error: Configuration file does not exists\n"
+		print("\n!Error: Configuration file does not exists\n")
 		return(True)
 	fh = open(config_file, "rU")
 	try:
@@ -98,71 +102,75 @@ def parse_config(config_file, inputs):
 				#print "KEY: %s, VAL: %s" % (key, val)
 				if key == 'REFERENCE':
 					inputs.reference = val
-					if inputs.verbose: print "[INFO] Parsed reference resource"
+					if inputs.verbose: print("[INFO] Parsed reference resource")
 					config_count = config_count + 1
 				elif key == 'GATK':
 					inputs.gatk = val
-					if inputs.verbose: print "[INFO] Parsed gatk resource"
-					config_count = config_count + 1
-				elif key == 'PICARD':
-					inputs.picard = val
-					if inputs.verbose: print "[INFO] Parsed picard resource"
+					if inputs.verbose: print("[INFO] Parsed gatk resource")
 					config_count = config_count + 1
 				elif key == 'SAMTOOLS':
 					inputs.samtools = val
-					if inputs.verbose: print "[INFO] Parsed samtools resource"
+					if inputs.verbose: print("[INFO] Parsed samtools resource")
 					config_count = config_count + 1
 				elif key ==	'BCFTOOLS':
 					inputs.bcftools = val
-					if inputs.verbose: print "[INFO] Parsed bcftools resource"
+					if inputs.verbose: print("[INFO] Parsed bcftools resource")
 					config_count = config_count + 1
 				elif key == 'SAMBAMBA':
 					inputs.sambamba = val
-					if inputs.verbose: print "[INFO] Parsed sambamba resource"
+					if inputs.verbose: print("[INFO] Parsed sambamba resource")
 					config_count = config_count + 1
 				elif key == 'BOWTIE2':
 					inputs.bowtie2 = val
-					if inputs.verbose: print "[INFO] Parsed bowtie2 resource"
+					if inputs.verbose: print("[INFO] Parsed bowtie2 resource")
 					config_count = config_count + 1
 				elif key == 'BOWTIE2BUILD':
 					inputs.bowtie2build = val
-					if inputs.verbose: print "[INFO] Parsed bowtie2build resource"
+					if inputs.verbose: print("[INFO] Parsed bowtie2build resource")
 					config_count = config_count + 1
 				elif key == 'BWA':
 					inputs.bwa = val
-					if inputs.verbose: print "[INFO] Parsed bwa resource"
+					if inputs.verbose: print("[INFO] Parsed bwa resource")
+					config_count = config_count + 1
+				elif key == 'FASTP':
+					inputs.fastp = val
+					if inputs.verbose: print("[INFO] Parsed fastp resource")
 					config_count = config_count + 1
 				elif key == 'CUTADAPT':
 					inputs.cutadapt = val
-					if inputs.verbose: print "[INFO] Parsed cutadapt resource"
+					if inputs.verbose: print("[INFO] Parsed cutadapt resource")
 					config_count = config_count + 1
 				elif key == 'SKEWER':
 					inputs.skewer = val
-					if inputs.verbose: print "[INFO] Parsed skewer resource"
+					if inputs.verbose: print("[INFO] Parsed skewer resource")
 					config_count = config_count + 1
 				elif key == 'AFTERQC':
 					inputs.afterqc = val
-					if inputs.verbose: print "[INFO] Parsed afterqc resource"
+					if inputs.verbose: print("[INFO] Parsed afterqc resource")
 					config_count = config_count + 1
 				elif key == 'STRELKABIN':
 					inputs.strelkabin = val
-					if inputs.verbose: print "[INFO] Parsed strelkabin resource"
+					if inputs.verbose: print("[INFO] Parsed strelkabin resource")
 					config_count = config_count + 1
 				elif key == 'MANTABIN':
 					inputs.mantabin = val
-					if inputs.verbose: print "[INFO] Parsed mantabin resource"
+					if inputs.verbose: print("[INFO] Parsed mantabin resource")
 					config_count = config_count + 1
 				elif key == 'KNOWNSITES':
 					inputs.knownsites = val.split(",")
-					if inputs.verbose: print "[INFO] Parsed knownsites resource"
+					if inputs.verbose: print("[INFO] Parsed knownsites resource")
+					config_count = config_count + 1
+				elif key == 'CNVKIT':
+					inputs.cnvkit = val
+					if inputs.verbose: print("[INFO] Parsed Cnvkit resource")
 					config_count = config_count + 1
 				else:
-					if inputs.verbose: print "[WARNING] Unrecognized resource in configuration file"
+					if inputs.verbose: print(f"[WARNING] Unrecognized resource in configuration file: {key}")
 	finally:
 		fh.close()
 	
 	if(config_count > 0):
-		if inputs.verbose: print "[INFO] Processed %d value pairs in configuration file" % (config_count)
+		if inputs.verbose: print("[INFO] Processed %d value pairs in configuration file" % (config_count))
 		return(False)
 	else:
 		return(None)
@@ -212,23 +220,23 @@ def get_inputs():
 
 	## Get Config Data ##
 	if parse_config(args.config, inputs) is None:
-		print "!Error in parsing configuration file\n"
+		print("!Error in parsing configuration file\n")
 		return(None)
 	
 	
 	if inputs.verbose:
-		print "\n"
-		print "\/" * 40
+		print("\n")
+		print("\/" * 40)
 		
-		for key, val in vars(inputs).iteritems():
+		for key, val in vars(inputs).items():
 			if(key == 'knownsites'):
 				for site in val:
-					print "\/ %s: %s" % ("KnownSite", site)
+					print("\/ %s: %s" % ("KnownSite", site))
 			else:
-				print "\/ %s: %s" % (key, val)
+				print("\/ %s: %s" % (key, val))
 				
-		print "\/" * 40
-		print "\n"
+		print("\/" * 40)
+		print("\n")
 		
 	return inputs
 
@@ -238,15 +246,15 @@ def get_inputs():
 
 if __name__ == '__main__':
 	
-	print "\n\n *** NGS Analysis ***\n\n"
+	print("\n\n *** NGS Analysis ***\n\n")
 
 	inputs = get_inputs();
 	if inputs is None:
-		print "!! Error in reading inputs"
+		print("!! Error in reading inputs")
 		sys.exit(1)
 
 	if check_inputs(inputs) is None:
-		print "[ERROR] Error in arguments"
+		print("[ERROR] Error in arguments")
 		sys.exit(1)
 		
 		
